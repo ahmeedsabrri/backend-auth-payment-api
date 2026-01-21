f2b26fd95360e9e98a2dbb8b35bf5057d0136eca
ahmeedsabrri <sabriahmeed1@gmail.com>
2026-01-21 11:37:17 +0100

    feat: enhance Swagger documentation for payment routes

--- src/routes/paymentRoutes.js
+++ src/routes/paymentRoutes.js
@@ -3,8 +3,65 @@ const router = express.Router();
 const { createPayment, getTransactions, getAllTransactions } = require('../controllers/paymentController');
 const { protect, authorize } = require('../middlewares/authMiddleware');
 
+/**
+ * @swagger
+ * tags:
+ *   name: Payments
+ *   description: Payment transaction management
+ */
+
+/**
+ * @swagger
+ * /payments:
+ *   post:
+ *     summary: Create a new payment transaction
+ *     tags: [Payments]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - amount
+ *               - currency
+ *             properties:
+ *               amount:
+ *                 type: number
+ *               currency:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Transaction created
+ *       400:
+ *         description: Invalid input
+ */
 router.post('/', protect, createPayment);
+
+/**
+ * @swagger
+ * /payments:
+ *   get:
+ *     summary: Get user transactions
+ *     tags: [Payments]
+ *     responses:
+ *       200:
+ *         description: List of transactions
+ */
 router.get('/', protect, getTransactions);
+
+/**
+ * @swagger
+ * /payments/all:
+ *   get:
+ *     summary: Get all transactions (Admin only)
+ *     tags: [Payments]
+ *     responses:
+ *       200:
+ *         description: List of all transactions
+ *       403:
+ *         description: Not authorized
+ */
 router.get('/all', protect, authorize('admin'), getAllTransactions);
 
 module.exports = router;
